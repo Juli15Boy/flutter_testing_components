2eeb6c0dd9d6a6c0c311b274ceeb12da1efca023
Juli <globaljuli@gmail.com>
2020-10-19 22:34:00 +0200

    Navigator push() and pop()

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/src/pages/alert_page.dart';
 
 import 'package:flutter_components/src/providers/menu_provider.dart';
 import 'package:flutter_components/src/utils/icon_string_util.dart';
@@ -21,20 +22,23 @@ class HomePage extends StatelessWidget {
       builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
         print('Builder');
         return ListView(
-          children: _getListItems(snapshot.data),
+          children: _getListItems(snapshot.data, context),
         );
       },
     );
   }
 
-  List<Widget> _getListItems(List<dynamic> data) {
+  List<Widget> _getListItems(List<dynamic> data, BuildContext context) {
     final List<Widget> options = [];
     data.forEach((opt) {
       final tempWidget = ListTile(
         title: Text(opt['text']),
         leading: getIcon(opt['icon']),
         trailing: Icon(Icons.keyboard_arrow_right, color: Colors.greenAccent),
-        onTap: () {},
+        onTap: () {
+          final route = MaterialPageRoute(builder: (context) => AlertPage());
+          Navigator.push(context, route);
+        },
       );
 
       options..add(tempWidget)..add(Divider());
