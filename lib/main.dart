d85f9639264206aad18e5862446f53b3db1a4a07
Juli <globaljuli@gmail.com>
2020-10-20 15:18:38 +0200

    Created Card Page and First Card Type

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_components/src/pages/alert_page.dart';
-import 'package:flutter_components/src/pages/avatar_page.dart';
-import 'package:flutter_components/src/pages/home_page.dart';
+import 'package:flutter_components/src/routes/routes.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,10 +12,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Components APP',
       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/': (BuildContext context) => HomePage(),
-        'alert': (BuildContext context) => AlertPage(),
-        'avatar': (BuildContext context) => AvatarPage(),
+      routes: getApplicationRoutes(),
+      onGenerateRoute: (settings) {
+        return MaterialPageRoute(
+            builder: (BuildContext context) => AlertPage());
       },
     );
   }
---
+++ lib/src/pages/card_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cards'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[_cardFirstType()],
+      ),
+    );
+  }
+
+  Widget _cardFirstType() {
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+            leading: Icon(
+              Icons.photo_album,
+              color: Colors.red,
+            ),
+            title: Text('Nais Card'),
+            subtitle: Text(
+                'The other day I was changing the subtitle of the card but I did it wrong, I think it\'s a good place to put it'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: ,
+            children: [
+              FlatButton(
+                child: Text('Cancel'),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () {},
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
