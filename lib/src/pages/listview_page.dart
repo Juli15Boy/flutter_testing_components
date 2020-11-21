b34f9939b29ad7648a3ec397e9a8e327beff2441
Juli N <globaljuli@gmail.com>
2020-11-21 21:42:39 +0100

    ListView

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class ListViewPage extends StatefulWidget {
   ListViewPage({Key key}) : super(key: key);
@@ -8,7 +9,34 @@ class ListViewPage extends StatefulWidget {
 }
 
 class _ListViewPageState extends State<ListViewPage> {
-  List<int> _numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  ScrollController _scrollController = new ScrollController();
+
+  List<int> _numberList = new List();
+  int _lastItem = 0;
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _addMoreImages();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        print("Final");
+        //_addMoreImages();
+        fetchData();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    //If we don't dispose(), each time we enter the page we will stack one more _scrollControler
+    _scrollController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,12 +44,18 @@ class _ListViewPageState extends State<ListViewPage> {
       appBar: AppBar(
         title: Text('Lists'),
       ),
-      body: _createList(),
+      body: Stack(
+        children: <Widget>[
+          _createList(),
+          _createLoading(),
+        ],
+      ),
     );
   }
 
   Widget _createList() {
     return ListView.builder(
+        controller: _scrollController,
         itemCount: _numberList.length,
         itemBuilder: (BuildContext context, index) {
           final imageIndex = _numberList[index];
@@ -32,5 +66,48 @@ class _ListViewPageState extends State<ListViewPage> {
                 'https://picsum.photos/500/300/?image=$imageIndex'),
           );
         });
+  }
+
+  _addMoreImages() {
+    for (var i = 0; i < 10; i++) {
+      _lastItem++;
+      _numberList.add(_lastItem);
+    }
+    setState(() {});
+  }
+
+  Future<Null> fetchData() async {
+    _isLoading = true;
+    setState(() {});
+    final duration = new Duration(seconds: 2);
+    return new Timer(duration, httpResponse);
+  }
+
+  void httpResponse() {
+    _isLoading = false;
+    _addMoreImages();
+    _scrollController.animateTo(
+      _scrollController.position.pixels + 100,
+      duration: Duration(milliseconds: 250),
+      curve: Curves.fastOutSlowIn,
+    );
+  }
+
+  Widget _createLoading() {
+    if (_isLoading) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircularProgressIndicator(),
+            ],
+          ),
+          SizedBox(height: 15.0),
+        ],
+      );
+    }
+    return Container();
   }
 }
